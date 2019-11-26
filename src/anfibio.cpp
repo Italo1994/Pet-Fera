a3d7e2fb7a307a3ed4543cdf9db356fb968d9d82
Larissa Jales <54720764+larissajales@users.noreply.github.com>
2019-11-25 23:36:45 -0300

    update

--- src/anfibio.cpp
+++ src/anfibio.cpp
@@ -1,5 +1,6 @@
 #include "anfibio.hpp"
 
+#construtor
 Anfibio::Anfibio()
 {
 
@@ -10,10 +11,12 @@ Anfibio::Anfibio(int m_total_de_mudas)
 	total_de_mudas = m_total_de_mudas;
 }
 
+#destrutor 
 Anfibio::~Anfibio()
 {
 
 }
+
 
 void Anfibio::setTotalDeMudas(int m_total_de_mudas)
 {
